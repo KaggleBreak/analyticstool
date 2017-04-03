791f8eea38d15be7a294e13d73064866cdcf8440
syleeie2310 <syleeie@gmail.com>
2017-04-03 21:33:57 +0900

    syleeie add

--- part3/R/0403/test4/app.R
+++ part3/R/0403/test4/app.R
@@ -13,12 +13,14 @@ sub = left_join(sub,teams,by=c('Team1'='Team_Id'))
 sub = left_join(sub,teams,by=c('Team2'='Team_Id'))
 
 sub2=sub
+dim(sub)
+sub$Pred
 sub2$Pred=1-sub$Pred
 sub2$Team_Name.x=sub$Team_Name.y
 sub2$Team_Name.y=sub$Team_Name.x
 
 sub=rbind(sub,sub2)
-
+head(sub)
 server <- function(input, output) {
   output$probability <- renderText({sub$Pred[sub$Team_Name.x == input$team1 & sub$Team_Name.y == input$team2]})
 }
